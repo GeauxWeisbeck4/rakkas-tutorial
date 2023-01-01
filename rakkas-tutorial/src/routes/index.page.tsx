e520fcdca6b0093a012dacda7b8f5209b36a10ad
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-31 19:02:08 -0500

    26 commits to go!

--- rakkas-tutorial/src/routes/index.page.tsx
+++ rakkas-tutorial/src/routes/index.page.tsx
@@ -21,14 +21,27 @@ const HomePage: Page = function HomePage() {
                     <li>Take a shower</li>
                 </ul>
             </div>
-            <h2>Images from the last year - foreshadowing?</h2>
+            <h2>Images from 2022 </h2>
             <img src="https://res.cloudinary.com/tar-heel-dev-studio/image/upload/v1671812077/svelte_rnxopq.png" alt="svelete"  />
             <img src="https://res.cloudinary.com/tar-heel-dev-studio/image/upload/v1670855326/fampic_qh5z1h.png" alt='family pic' className={css.familyPic}/>
+            <img src="https://res.cloudinary.com/tar-heel-dev-studio/image/upload/v1670752723/dukeunc_aybimr.jpg" alt='kill shot unc-duke' className={css.uncPic} />
         </div>
         <div className={css.updateSection}>
-            <h1>This is the update section</h1>
-            <p>I will keep you updated on everything I am doing tonight if you visit here. I am going to deploy this to multiple spaces so everyone can find
-            me and stay up to date with my New Years adventures, whatever I am working on!</p>
+            <h1>I gave it all I had... It wasn't enough...</h1>
+            <p>If this year wasn't the hardest year of my life then I missed something. I didn't have a job the whole year - a couple part-time gigs that didn't work out, but otherwise
+            fucking nothing. I'm depressed when thinking back on how many times I thought I had the job, only to be turned down. I applied for easily over 1,000 jobs with about 10 applications per day
+            average I'd say. Some day it'd be 50 apps out of frustration. </p>
+            <br />
+            <p>I seemed to hit freelancers gold at the end of the year, but that still wasn't enough. I'm the reason we're headed for our second eviction - I've done everything I can, but its not good enough.
+            I really don't know how I respond to such a fucked up and bad year. It's kind of like, where do I go from here? One option, I've tried before and I know that I can't do that - not even an option. It's horrible, dark,
+            cold, and cruel to the people who love you. The other option is to keep going - its the only one I have. The year won't start off easy - we'll likely be homeless for a bit trying to figure out what the fuck to even do.</p>
+            <br />
+            <br />
+            <p>
+            It's scary - but I can't be scared. People, animals, and creditors (haha) rely on me to do the right thing and keep coming back to try again. And I'm sooo close. I got offered a job even. I just couldn't take it because of
+            where it is located. I need some help - likely a couple miracles. I will post a way for others to help, but it is much needed so we don't freeze to death in the cold. I just don't know what else to do. You have no idea how grateful we'd all
+            be - we'd be in your debt for saving our lives because I'm not sure how we're going to do it on a $1,000 every two week salary. We'll do the right thing too, like we've tried to do all year. Sometimes things just go wrong in life, a lot - and for
+            a long time it seems. But I have faith that this is the year things change and Lisa and I finally can find prosperity and also get married, like we've tried to do for two years now. Thank you for reading and I hope you enjoy the site - Happy New Years!</p>
             <h3>I am currently working on:</h3>
             <p>RakkasJS Tutorial!</p>
             <img src="https://res.cloudinary.com/dvsjspnnz/image/upload/v1664969406/cld-sample-2.jpg" alt="beautiful mountains" className={css.updatePic} />
